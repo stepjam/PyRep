11acd97abc0b0793f37657f5c52c597d0f26c12e
Doug Morrison <doug.morrison91@gmail.com>
2019-07-04 17:12:18 +1000

    Add simConvexDecompose

--- pyrep/objects/shape.py
+++ pyrep/objects/shape.py
@@ -197,6 +197,109 @@ class Shape(Object):
         """
         return vrep.simGetShapeMesh(self._handle)
 
+    def get_convex_decomposition(self, morph=True, same=False, use_vhacd=False,
+                                 individual_meshes=False,
+                                 hacd_extra_points=True, hacd_face_points=True,
+                                 hacd_min_clusters=1, hacd_tri_target=500,
+                                 hacd_max_vertex=200, hacd_max_iter=4,
+                                 hacd_max_concavity=100, hacd_max_dist=30,
+                                 hacd_cluster_thresh=0.25,
+                                 vhacd_pca=False, vhacd_tetrahedron=False,
+                                 vhacd_res=100000, vhacd_depth=20,
+                                 vhacd_plane_downsample=4,
+                                 vhacd_hull_downsample=4,
+                                 vhacd_max_vertex=64, vhacd_concavity=0.0025,
+                                 vhacd_alpha=0.05, vhacd_beta=0.05,
+                                 vhacd_gamma=0.00125, vhacd_min_vol=0.0001
+                                 ) -> 'Shape':
+        """
+        Compute the convex decomposition of the shape using HACD or V-HACD
+         algorithms
+        :param morph: The shape will be morphed into its convex decomposition.
+            Otherwise a new shape will be created.
+        :param same: Use the same parameters as the last call to the function.
+        :param use_vhacd: Use V-HACD algorithm.
+        :param individual_meshes: Each individual mesh of a compound shape will
+            be handled on its own during decomposition, otherwise the
+            compound shape is considered as a single mesh.
+        :param hacd_extra_points: HACD: Extra points will be added when
+            computing the concavity.
+        :param hacd_face_points: HACD: Faces points will be added when computing
+            the concavity.
+        :param hacd_min_clusters: HACD: Minimum number of clusters to generate.
+        :param hacd_tri_target: HACD: Targeted number of triangles of the
+            decimated mesh.
+        :param hacd_max_vertex: HACD: Maximum number of vertices for each
+            generated convex hull.
+        :param hacd_max_iter: HACD: Maximum number of iterations.
+        :param hacd_max_concavity: HACD: The maximum allowed concavity.
+        :param hacd_max_dist: HACD: The maximum allowed distance to get convex
+            clusters connected.
+        :param hacd_cluster_thresh: HACD: The threshold to detect small
+            clusters, expressed as a fraction of the total mesh surface.
+        :param vhacd_pca: V-HACD: Enable PCA.
+        :param vhacd_tetrahedron: V-HACD: Tetrahedron-based approximate convex
+            decomposition.  Otherwise, voxel-based decomposition is used.
+        :param vhacd_res: V-HACD: Resolution (10000-64000000)
+        :param vhacd_depth: V-HACD: Depth (1-32)
+        :param vhacd_plane_downsample: V-HACD: Plane downsampling (1-16)
+        :param vhacd_hull_downsample: V-HACD: Convex hull downsampling (1-16)
+        :param vhacd_max_vertex: V-HACD: Maximum number of vertices per convex
+            hull (4-1024)
+        :param vhacd_concavity: V-HACD: Concavity (0.0-1.0)
+        :param vhacd_alpha: V-HACD: Alpha (0.0-1.0)
+        :param vhacd_beta: V-HACD: Beta (0.0-1.0)
+        :param vhacd_gamma: V-HACD: Gamma (0.0-1.0)
+        :param vhacd_min_vol: V-HACD: Minimum volume per convex hull (0.0-0.01)
+        :return: Convex Decomposition of the shape.
+        """
+        options = 0
+        if morph:
+            options |= 1
+        if same:
+            options |= 4
+        if hacd_extra_points:
+            options |= 8
+        if hacd_face_points:
+            options |= 16
+        if individual_meshes:
+            options |= 32
+        if use_vhacd:
+            options |= 128
+        if vhacd_pca:
+            options |= 256
+        if vhacd_tetrahedron:
+            options |= 512
+
+        int_params = [
+            hacd_min_clusters,          # [0]
+            hacd_tri_target,            # [1]
+            hacd_max_vertex,            # [2]
+            hacd_max_iter,              # [3]
+            0,                          # [4]
+            vhacd_res,                  # [5]
+            vhacd_depth,                # [6]
+            vhacd_plane_downsample,     # [7]
+            vhacd_hull_downsample,      # [8]
+            vhacd_max_vertex            # [9]
+        ]
+
+        float_params = [
+            hacd_max_concavity,         # [0]
+            hacd_max_dist,              # [1]
+            hacd_cluster_thresh,        # [2]
+            0.0,                        # [3]
+            0.0,                        # [4]
+            vhacd_concavity,            # [5]
+            vhacd_alpha,                # [6]
+            vhacd_beta,                 # [7]
+            vhacd_gamma,                # [8]
+            vhacd_min_vol               # [9]
+        ]
+
+        return Shape(vrep.simConvexDecompose(self.get_handle(), options,
+                                             int_params, float_params))
+
     def get_texture(self):
         """Retrieves the texture from the shape.
         :return: The texture associated with this object.
@@ -245,5 +348,3 @@ class Shape(Object):
         vrep.simSetShapeTexture(
             self.get_handle(), texture.get_texture_id(), mapping_mode.value,
             options, list(uv_scaling), position, orientation)
-
-
