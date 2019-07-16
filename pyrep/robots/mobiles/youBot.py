be028d0ab5e1b3eac141469bd9490c585cfff9c1
gn3112 <georgesnomicos@gmail.com>
2019-07-17 00:04:46 +0100

    changed args order init

--- pyrep/robots/mobiles/youBot.py
+++ pyrep/robots/mobiles/youBot.py
@@ -4,4 +4,4 @@ from pyrep.robots.mobiles.mobile import Mobile
 class youBot(Mobile):
 
     def __init__(self, count: int = 0, distance_from_target: float = 0):
-        super().__init__(count, 'youBot', 'omnidirectional', None, 4, 6, 0.035, distance_from_target)
+        super().__init__(count, distance_from_target, 'youBot', 'omnidirectional', None, 4, 6, 0.035)
