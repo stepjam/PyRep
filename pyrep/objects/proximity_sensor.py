118ffd35dcfc4ec52b8c0ac7cdafbe31637e1ca8
Souphis <gbartyzel@hotmail.com>
2019-12-06 01:05:36 +0100

    Add distance acquisition method to ProximitySensor

--- pyrep/objects/proximity_sensor.py
+++ pyrep/objects/proximity_sensor.py
@@ -1,3 +1,4 @@
+import math
 from pyrep.backend import vrep
 from pyrep.objects.object import Object
 from pyrep.const import ObjectType
@@ -12,6 +13,16 @@ class ProximitySensor(Object):
 
     def _get_requested_type(self) -> ObjectType:
         return ObjectType.PROXIMITY_SENSOR
+
+    def measure(self) -> float:
+        """Measure the distance between sensor and first detected object.
+
+        :return: Float distance to the first detected object
+        """
+        state, _, points, _ = vrep.simReadProximitySensor(self._handle)
+        if state:
+            return math.sqrt(points[0] ** 2 + points[1] ** 2 + points[2] ** 2)
+        return -1.0
 
     def is_detected(self, obj: Object) -> bool:
         """Checks whether the proximity sensor detects the indicated object.
