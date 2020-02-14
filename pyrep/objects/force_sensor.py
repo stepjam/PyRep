b269fe2b9fb0d4f4223c9c5455b61b88becff5e5
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-14 16:29:20 +0000

    Add ForceSensor.create

--- pyrep/objects/force_sensor.py
+++ pyrep/objects/force_sensor.py
@@ -11,6 +11,15 @@ class ForceSensor(Object):
     def _get_requested_type(self) -> ObjectType:
         return ObjectType.FORCE_SENSOR
 
+    @classmethod
+    def create(cls, sensor_size=0.01) -> 'ForceSensor':
+        options = 0  # force and torque threshold are disabled
+        intParams = [0, 0, 0, 0, 0]
+        floatParams = [sensor_size, 0, 0, 0, 0]
+        handle = sim.simCreateForceSensor(options=0, intParams=intParams,
+                                          floatParams=floatParams, color=None)
+        return cls(handle)
+
     def read(self) -> Tuple[List[float], List[float]]:
         """Reads the force and torque applied to a force sensor.
 
