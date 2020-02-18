70893b029749823f0b0de72ee4c7268374b94f14
wawa <736499127wawa@gmail.com>
2020-02-18 00:39:52 +0000

    add a simple quadricopter

--- examples/example_drone.py
+++ examples/example_drone.py
@@ -7,13 +7,14 @@ from pyrep.const import PrimitiveShape
 import numpy as np
 
 LOOPS = 10
-SCENE_FILE = join(dirname(abspath(__file__)), 'cooperative_transportation_uav.ttt')
+SCENE_FILE = join(dirname(abspath(__file__)), 'example_drone.ttt')
 pr = PyRep()
 pr.launch(SCENE_FILE, headless=False)
 pr.start()
 
 agent = Quadricopter()
-# We could have made this target in the scene, but lets create one dynamically
+
+# create a Cuboid
 target = Shape.create(type=PrimitiveShape.CUBOID,
                       size=[0.8, 0.8, 0.8],
                       color=[1.0, 0.1, 0.1],
@@ -21,21 +22,22 @@ target = Shape.create(type=PrimitiveShape.CUBOID,
 
 position_min, position_max = [-0.4, -0.4, 1.5], [0.4, 0.4, 1.5]
 
-
-#robot_DIR = path.join(path.dirname(path.abspath(__file__)), 'models')
-#m = pr.import_model(path.join(robot_DIR, 'Quadricopter_rope.ttm'))
-
-
 for i in range(LOOPS):
 
-    # Get a random position within a cuboid and set the target position
+    # Get a random position for the drone
     pos = list(np.random.uniform(position_min, position_max))
-    agent.set_3d_pose(pos+[0.0,0.0,0.0])
+    agent.set_3d_pose(pos+[0.0,0.0,0.0]) # The orientation here is all zeros
+
+    #Set the position for the cuboid
     target.set_position([0.0,0.0,0.4])
 
     for j in np.arange(100):
+
+        # The velcities set here are just rubbish.
+        # YOU SHOULD DEVELOP YOUR OWN VELOCITY CONTROL METHOD
         agent.set_propller_velocity(5.5, 5.5, 5.5, 5.5)
 
+        # Power each propeller
         agent.control_propeller_thrust(1)
         agent.control_propeller_thrust(2)
         agent.control_propeller_thrust(3)
