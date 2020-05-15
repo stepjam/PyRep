b559cb6cefde5c9a5647e76fea7a9743e2884a87
stephen <slj12@ic.ac.uk>
2020-05-15 14:48:35 +0100

    Allow relative_to in arm paths/ik.

--- tests/test_arms_and_configuration_paths.py
+++ tests/test_arms_and_configuration_paths.py
@@ -99,6 +99,14 @@ class TestArmsAndConfigurationPaths(TestCore):
             waypoint.get_position(), waypoint.get_orientation())
         self.assertIsNotNone(path)
 
+    def test_get_linear_path_relative(self):
+        arm = Panda()
+        waypoint = Dummy('Panda_waypoint')
+        path = arm.get_linear_path(
+            waypoint.get_position(), waypoint.get_orientation(),
+            relative_to=arm.get_tip())
+        self.assertIsNotNone(path)
+
     def test_get_nonlinear_path(self):
         arm = Panda()
         waypoint = Dummy('Panda_waypoint')
