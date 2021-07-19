9bcf2c1239ec18b3eb537693f60a6c232d789070
Stephen James <stepjamuk@gmail.com>
2021-07-19 08:37:11 -0700

    Fix usrset file check during install.

--- setup.py
+++ setup.py
@@ -26,11 +26,15 @@ def get_version(rel_path):
 # Temp fix for CoppeliaSim 4.1
 if 'COPPELIASIM_ROOT' not in os.environ:
     raise RuntimeError('COPPELIASIM_ROOT not defined.')
-with open(os.path.join(os.environ['COPPELIASIM_ROOT'], 'system', 'usrset.txt'), 'r') as f:
-    usrset = f.read()
+
+usrset_file = os.path.join(os.environ['COPPELIASIM_ROOT'], 'system', 'usrset.txt')
+usrset = ''
+if os.path.isfile(usrset_file):
+    with open(usrset_file, 'r') as f:
+        usrset = f.read()
 
 if 'allowOldEduRelease' not in usrset:
-    with open(os.path.join(os.environ['COPPELIASIM_ROOT'], 'system', 'usrset.txt'), 'a') as f:
+    with open(usrset_file, 'a+') as f:
         f.write('\nallowOldEduRelease=7501\n')
 
 
