ce1708c93b87b78143885321d2e4709461dabdbb
ademiadeniji <ademiadeniji7@gmail.com>
2023-01-20 21:49:29 +0000

    try to integrate xarm

---
+++ pyrep/robots/end_effectors/xarm_gripper.py
@@ -0,0 +1,9 @@
+from pyrep.robots.end_effectors.gripper import Gripper
+
+class xarmGripper(Gripper):
+
+    def __init__(self, count: int = 0):
+        super().__init__(count, 'JacoHand',
+                         ['JacoHand_joint1_finger1',
+                          'JacoHand_joint1_finger2',
+                          'JacoHand_joint1_finger3'])
