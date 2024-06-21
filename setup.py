eb4abf1571b0bb1425d81f191355236a311dae8d
Eugene Teoh <50418168+eugeneteoh@users.noreply.github.com>
2024-06-21 20:56:21 +0100

    Fix cffi dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 import codecs
 import os
 import os.path
-from os.path import join
 
 from setuptools import setup
-
-import cffi_build.cffi_build as cffi_build
 
 
 def read(rel_path):
@@ -38,27 +35,29 @@ if 'allowOldEduRelease' not in usrset:
         f.write('\nallowOldEduRelease=7501\n')
 
 
-setup(name='PyRep',
-      # Version A.B.C.D.
-      # A.B.C info corresponds to the CoppeliaSim version needed.
-      # D info corresponds to the PyRep version.
-      version=get_version("pyrep/__init__.py"),
-      description='Python CoppeliaSim wrapper',
-      author='Stephen James',
-      author_email='slj12@ic.ac.uk',
-      url='https://www.doc.ic.ac.uk/~slj12',
-      packages=['pyrep',
-                'pyrep.backend',
-                'pyrep.objects',
-                'pyrep.sensors',
-                'pyrep.robots',
-                'pyrep.robots.arms',
-                'pyrep.robots.end_effectors',
-                'pyrep.robots.mobiles',
-                'pyrep.robots.configuration_paths',
-                'pyrep.textures',
-                'pyrep.misc',
-                ],
-      ext_modules=[cffi_build.ffibuilder.distutils_extension(
-          join('build', 'pyrep', 'backend'))],
-      )
+setup(
+    name='PyRep',
+    # Version A.B.C.D.
+    # A.B.C info corresponds to the CoppeliaSim version needed.
+    # D info corresponds to the PyRep version.
+    version=get_version("pyrep/__init__.py"),
+    description='Python CoppeliaSim wrapper',
+    author='Stephen James',
+    author_email='slj12@ic.ac.uk',
+    url='https://www.doc.ic.ac.uk/~slj12',
+    packages=['pyrep',
+        'pyrep.backend',
+        'pyrep.objects',
+        'pyrep.sensors',
+        'pyrep.robots',
+        'pyrep.robots.arms',
+        'pyrep.robots.end_effectors',
+        'pyrep.robots.mobiles',
+        'pyrep.robots.configuration_paths',
+        'pyrep.textures',
+        'pyrep.misc',
+        ],
+    setup_requires=["cffi<1.16"],
+    cffi_modules=["cffi_build/cffi_build.py:ffibuilder"],
+    install_requires=["cffi<1.16"],
+)
